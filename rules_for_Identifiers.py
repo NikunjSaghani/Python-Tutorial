ba6f08f96bc7d6190aaadd69b197adecf0cf1d52
Nikunj Sanghani <nikunjsanghaniedu@gmail.com>
2024-05-21 15:28:41 +0200

    🏷️ Rules for Identifiers
    What is an Identifier?
    Identifiers are names given to entities like variables, functions, classes, etc. They help differentiate one entity from another.
    
    Real-Life Example:
    Identifiers are like names given to people or places. Just as you have a unique name that identifies you, variables and functions in Python have unique names (identifiers).
    
    Key Points:
    -> Must begin with a letter (A-Z, a-z) or an underscore (_).
    -> Can include letters, digits (0-9), and underscores.
    -> Cannot be a reserved word (like if, while, class).
    -> Case-sensitive.

---
+++ rules_for_Identifiers.py
@@ -0,0 +1,6 @@
+# Valid and invalid identifiers
+_valid_name = "John"
+valid123 = 123
+# 123invalid = "Invalid"  # This will cause an error
+# if = 5  # This will cause an error because 'if' is a reserved word
+print("Valid Name: {_valid_name}\nValid Number: {valid123}")
